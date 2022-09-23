afd8c9e4a72ce642e4c13f3449d7b1cd1dda474b
sculove <sculove.pe.kr@navercorp.com>
2022-09-23 09:11:21 +0900

    :wrench: github 포팅

--- deploy.js
+++ deploy.js
@@ -1,6 +1,8 @@
 const ghpages = require('gh-pages')
 
-ghpages.publish('build', function (err) {
+ghpages.publish('build', {
+    remote: 'github'
+}, function (err) {
     // eslint-disable-next-line no-console
     err && console.error(err)
 })
